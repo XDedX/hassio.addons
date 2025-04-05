60eec736cffc7c292c408d6e81a34030e0f295db
XDedX <1410714@gmail.com>
2025-04-05 15:55:47 +0300

    Update oalpr.py

--- addon-oalpr/oalpr/oalpr.py
+++ addon-oalpr/oalpr/oalpr.py
@@ -1,6 +1,7 @@
 import logging
 import signal
 import sys
+import os
 
 from flask import Flask
 from openalpr import Alpr
@@ -11,10 +12,13 @@ IMAGE_FILE = '/config/oalpr/data/plate.jpg'
 logging.basicConfig(filename=LOG_FILE, filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
 
 app = Flask(__name__)
-lpr = Alpr("us", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
-lpr.set_default_region("us")
+
+# Читаем страну из переменной окружения, по умолчанию "us"
+COUNTRY = os.getenv("COUNTRY", "ua")
+lpr = Alpr(COUNTRY, "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
+lpr.set_default_region(COUNTRY)  # Устанавливаем регион по умолчанию
 if not lpr.is_loaded():
-    logging.error("Error loading OpenALPR")
+    logging.error(f"Error loading OpenALPR with country {COUNTRY}")
     sys.exit(1)
 
 
@@ -30,7 +34,7 @@ def do_it():
     if recognized_plates == "":
         text = "No plates recognized"
     else:
-        text = "Recognized plates {}".format(recognized_plates)
+        text = f"Recognized plates {recognized_plates}"
 
     logging.info(text)
 
@@ -45,7 +49,7 @@ def signal_handler(sig, frame):
 
 try:
     signal.signal(signal.SIGINT, signal_handler)
-    logging.info('ALPR loaded. Ready to recognize')
+    logging.info(f'ALPR loaded with country {COUNTRY}. Ready to recognize')
     app.run(host='0.0.0.0')
 except Exception as e:
     logging.error("Exception occurred", exc_info=True)
