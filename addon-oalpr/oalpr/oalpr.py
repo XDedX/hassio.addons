4d8812afacfcffcd83fd55f4ab196193e79f3f69
XDedX <1410714@gmail.com>
2025-04-05 18:01:16 +0300

    Update oalpr.py

--- addon-oalpr/oalpr/oalpr.py
+++ addon-oalpr/oalpr/oalpr.py
@@ -13,31 +13,31 @@ logging.basicConfig(filename=LOG_FILE, filemode='w', format='%(asctime)s - %(mes
 
 app = Flask(__name__)
 
-# Читаем страну из переменной окружения, по умолчанию "us"
-COUNTRY = os.getenv("COUNTRY", "ua")
+COUNTRY = os.getenv("COUNTRY", "us")
 lpr = Alpr(COUNTRY, "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
-lpr.set_default_region(COUNTRY)  # Устанавливаем регион по умолчанию
+lpr.set_default_region(COUNTRY)
 if not lpr.is_loaded():
     logging.error(f"Error loading OpenALPR with country {COUNTRY}")
-    sys.exit(1)
+    sys.exit(1)  # Завершаем процесс при ошибке
 
 
 @app.route('/plate')
 def do_it():
     logging.info("Starting recognition")
+    try:
+        lpr_results = lpr.recognize_file(IMAGE_FILE)
+        recognized_plates = ""
+        for result in lpr_results["results"]:
+            recognized_plates = result["plate"]
 
-    lpr_results = lpr.recognize_file(IMAGE_FILE)
-    recognized_plates = ""
-    for result in lpr_results["results"]:
-        recognized_plates = result["plate"]
-
-    if recognized_plates == "":
-        text = "No plates recognized"
-    else:
-        text = f"Recognized plates {recognized_plates}"
-
+        if recognized_plates == "":
+            text = "No plates recognized"
+        else:
+            text = f"Recognized plates {recognized_plates}"
+    except Exception as e:
+        text = f"Error recognizing plate: {str(e)}"
+        logging.error(text)
     logging.info(text)
-
     return text
 
 
@@ -47,9 +47,6 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 
-try:
-    signal.signal(signal.SIGINT, signal_handler)
-    logging.info(f'ALPR loaded with country {COUNTRY}. Ready to recognize')
-    app.run(host='0.0.0.0')
-except Exception as e:
-    logging.error("Exception occurred", exc_info=True)
+signal.signal(signal.SIGINT, signal_handler)
+logging.info(f'ALPR loaded with country {COUNTRY}. Ready to recognize')
+app.run(host='0.0.0.0')
